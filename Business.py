e545515fd307bb7aac5c34bd2072947cf210c541
Json Brown <136649745+MonotonicIodine@users.noreply.github.com>
2025-03-05 09:49:11 -0600

    Update Business.py
    Starter code

--- Business.py
+++ Business.py
@@ -7,3 +7,18 @@
 #        Attributes: Product categories, customer capacity, payment methods
 #    Food:
 #        Attributes: Cuisine type, seating capacity, meal plans accepted
+Class Business:
+def __init__(self, name, location, __revenue, operating_hours, floor_spaces)
+    self.name = name
+    self.location = location
+    self.__revenue = __revenue
+    self.operating_hours = operating_hours
+    self.floor_spaces = floor_spaces
+# possible methods here
+
+Class Bookstore(Business):
+def __init__(self, __inventory_size, employee_count, university_merchandise)
+    super.init().__init__()
+    self.__inventory_size = __inventory_size
+    self.employee_count = employee_count
+    self.university_merchandise = university_merchandise 
