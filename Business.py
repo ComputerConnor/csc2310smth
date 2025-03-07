40e119040f02b14114bce5a004aa3adb74c4c15d
unknown <colemanbrown1086@gmail.com>
2025-03-06 23:51:26 -0600

    Finished All Business Classes and children classes. Test file if needed to make sure inherited classes work

--- Business.py
+++ Business.py
@@ -1,24 +1,34 @@
 #MonotonicIodine
-#Business
-#    Attributes: Name, location, revenue, operating hours, floor spaces
-#    Bookstore:
-#        Attributes: Inventory size, number of employees, affiliated university merchandise
-#    Retail:
-#        Attributes: Product categories, customer capacity, payment methods
-#    Food:
-#        Attributes: Cuisine type, seating capacity, meal plans accepted
-Class Business:
-def __init__(self, name, location, __revenue, operating_hours, floor_spaces)
-    self.name = name
-    self.location = location
-    self.__revenue = __revenue
-    self.operating_hours = operating_hours
-    self.floor_spaces = floor_spaces
-# possible methods here
 
-Class Bookstore(Business):
-def __init__(self, __inventory_size, employee_count, university_merchandise)
-    super.init().__init__()
-    self.__inventory_size = __inventory_size
-    self.employee_count = employee_count
-    self.university_merchandise = university_merchandise 
+class Business:
+    def __init__(self, name, location, __revenue, operating_hours, floor_spaces):
+        self.name = name
+        self.location = location
+        self.__revenue = __revenue
+        self.operating_hours = operating_hours
+        self.floor_spaces = floor_spaces
+        # possible methods here
+
+class Bookstore(Business):
+    def __init__(self,name, location, __revenue, operating_hours, floor_spaces, __inventory_size, employee_count, university_merchandise):
+        super().__init__(name, location, __revenue, operating_hours, floor_spaces)
+        self.__inventory_size = __inventory_size
+        self.employee_count = employee_count
+        self.university_merchandise = university_merchandise
+        # possible methods here
+
+class Retail(Business):
+    def __init__(self, name, location, __revenue, operating_hours, floor_spaces, product_category, customer_capacity, payment_methods):
+        super().__init__(name, location, __revenue, operating_hours, floor_spaces)
+        self.product_category = product_category
+        self.customer_capacity = customer_capacity
+        self.payment_methods = payment_methods
+        # possible methods here
+
+class Food(Business):
+    def __init__(self, name, location, __revenue, operating_hours, floor_spaces,  cuisine_type, seating_capacity, accepted_meal_plans):
+        super().__init__(name, location, __revenue, operating_hours, floor_spaces)
+        self.cuisine_type = cuisine_type
+        self.seating_capacity = seating_capacity
+        self.accepted_meal_plans = accepted_meal_plans
+        # possible methods here
---
+++ test.py
@@ -0,0 +1,9 @@
+from Business import Bookstore,Retail,Food
+
+# Console testing functionality
+
+books_building = Bookstore("bookmillion Store", "123 Main St", 1003000, "9-5", 10, 45, 34, "ttu")
+print(books_building.location)
+
+books_building2 = Retail("retailmillion Store", "1253 Main St", 10000, "3-5", 10, 45, 34, "cash")
+print(books_building2.location)
